ebcc7441d5a5a5c11773d0d6960fa202d28d145b
ermyas7 <ermyas.fakadu@gmail.com>
2019-01-02 13:17:24 +0530

    group test using describe

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,27 +1,35 @@
 const expect = require('expect')
 const utils = require('./utils')
-it('should add two numbers', () => {
-  var res = utils.add(15, 32);
-  expect(res).toBe(47).toBeA('number');
-})
 
-it('should async add 2 numbers', (done) => {
-  utils.asyncAdd(12, 11, (sum) => {
-    expect(sum).toBe(23).toBeA('number');
-    done();
-  } )
-})
+describe('utils', () => {
 
-it("should square a number", () => {
-  var res = utils.square(9);
-  expect(res).toBe(81).toBeA('number');
-})
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      var res = utils.add(15, 32);
+      expect(res).toBe(47).toBeA('number');
+    })
 
-it("should async square a number", (done) => {
-  utils.asyncSquare(9, (res) =>{
-    expect(res).toBe(81).toBeA('number');
-    done();
-  } )
+    it('should async add 2 numbers', (done) => {
+      utils.asyncAdd(12, 11, (sum) => {
+        expect(sum).toBe(23).toBeA('number');
+        done();
+      } )
+    })
+  })
+
+  describe('#square', () => {
+    it("should square a number", () => {
+      var res = utils.square(9);
+      expect(res).toBe(81).toBeA('number');
+    })
+
+    it("should async square a number", (done) => {
+      utils.asyncSquare(9, (res) =>{
+        expect(res).toBe(81).toBeA('number');
+        done();
+      } )
+    })
+  })
 })
 
 it("should set firstName and lastName", () => {
