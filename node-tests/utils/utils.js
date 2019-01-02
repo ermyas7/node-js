f0bdd188d0085aa4ec083c13ea7594586aa58a92
ermyas7 <ermyas.fakadu@gmail.com>
2019-01-02 12:25:14 +0530

    testing with mocha and expect complete

---
+++ node-tests/utils/utils.js
@@ -0,0 +1,20 @@
+module.exports.add = (a, b) => a + b;
+module.exports.square = x => x*x;
+module.exports.setName = (user, fullName) => {
+  let names = fullName.split(' ');
+  user.firstName = names[0];
+  user.lastName = names[1];
+  return user;
+}
+
+module.exports.asyncAdd = (a, b, callback) => {
+  setTimeout(()=>{
+    callback(a + b);
+  }, 1000);
+};
+
+module.exports.asyncSquare = (x, callback) => {
+  setTimeout(()=>{
+    callback(x*x);
+  }, 1000);
+};
---
+++ node-tests/utils/utils.test.js
@@ -0,0 +1,36 @@
+const expect = require('expect')
+const utils = require('./utils')
+it('should add two numbers', () => {
+  var res = utils.add(15, 32);
+  expect(res).toBe(47).toBeA('number');
+})
+
+it('should async add 2 numbers', (done) => {
+  utils.asyncAdd(12, 11, (sum) => {
+    expect(sum).toBe(23).toBeA('number');
+    done();
+  } )
+})
+
+it("should square a number", () => {
+  var res = utils.square(9);
+  expect(res).toBe(81).toBeA('number');
+})
+
+it("should async square a number", (done) => {
+  utils.asyncSquare(9, (res) =>{
+    expect(res).toBe(81).toBeA('number');
+    done();
+  } )
+})
+
+it("should set firstName and lastName", () => {
+  var user = {
+    age: 23,
+    location: 'silchar'
+  };
+  var res = utils.setName(user, 'Ermyas Fekadu');
+  expect(res).toInclude({firstName: 'Ermyas'});
+  expect(res).toInclude({lastName: 'Fekadu'});
+  expect(res).toBeA('object');
+})
