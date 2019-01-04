30c1ea27472faa14841d67469f013a9a202533be
ermyas7 <ermyas.fakadu@gmail.com>
2019-01-04 17:13:10 +0530

    add post todo route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,3 +1,20 @@
-const mongoose = './db/mongoose',
-      {Todo}   = './models/todo',
-      {User}   = './models/todo'; 
+const mongoose = require('./db/mongoose'),
+      {Todo}   = require('./models/todo'),
+      {User}   = require('./models/todo');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('testing')
+})
+
+app.post('/todos', (req, res) => {
+  console.log(req.body.text)
+  res.send(req.body.text)
+})
+
+app.listen(3000, () => console.log('server running on port 3000'));
