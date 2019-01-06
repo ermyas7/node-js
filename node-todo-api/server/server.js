c1ba1804f75b358fbee738f06b7868abb36a425f
ermyas7 <ermyas.fakadu@gmail.com>
2019-01-06 20:01:35 +0530

    Add Get /todos route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -21,6 +21,14 @@ app.post('/todos', (req, res) => {
 
 });
 
+//get all todos
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({
+      todos
+    })
+  }).catch((err) => res.status(400).send(err));
+})
 app.listen(3000, () => console.log('server running on port 3000'));
 
 module.exports = {app};
