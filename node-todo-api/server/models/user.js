b88185a29313f8781e2d87f9345642bda17438c9
ermyas7 <ermyas.fakadu@gmail.com>
2019-01-21 20:04:13 +0530

    add user model

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -1,18 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const validator = require('validator')
 
 var User = mongoose.model('User', {
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
+    minlength: 1,
+    unique: true,
+    validate:{
+      validator: validator.isEmail,
+      message:`{VALUE} is not a valid email!`
+    }
   },
-  name: {
+  password: {
     type: String,
     required: true,
-    trim: true,
-    minlength: 1
-  }
+    minlength: 6
+  },
+  token:[{
+    acess:{
+      type: String,
+      required: true
+    },
+    tokens:{
+      type: String,
+      require: true
+    }
+  }]
 });
 
 
