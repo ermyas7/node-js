8f58b6a828672696f7a6fe891b6dc231daddaa82
ermyas7 <ermyas.fakadu@gmail.com>
2019-01-27 10:02:12 +0530

    Add tests for POST /users/login

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -254,3 +254,75 @@ beforeEach(populateTodos);
           .end(done)
         })
       })
+
+
+      //////////////////////////////////////////////////////
+      /////////////////////////////////////////////////////
+      ////////////  /users/login test  ////////////////////
+      ////////////////////////////////////////////////////
+      ///////////////////////////////////////////////////
+
+describe('POST /users/login', () => {
+  //test for valid credentials
+  it('should login user and create auth token', (done) => {
+
+    request(app)
+    .post('/users/login')
+    .send({
+      email: users[1].email,
+      password: users[1].password
+    })
+    .expect(200)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toExist()
+    })
+    .end((err, res) => {
+      if(err){
+        done(err)
+      }
+
+      User.findById(users[1]._id)
+      .then((user) => {
+        expect(user.tokens[0]).toInclude({
+              access: 'auth',
+              token: res.headers['x-auth']
+            })
+
+        done()
+      })
+      .catch(err => done(err))
+
+    })
+  })
+
+  //for invalid findByCredentials
+
+    it('should reject invalid login', (done) => {
+
+      request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password + '1'
+      })
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist()
+      })
+      .end((err, res) => {
+        if(err){
+          done(err)
+        }
+
+        User.findById(users[1]._id)
+        .then((user) => {
+          expect(user.tokens.length).toBe(0);
+
+          done()
+        })
+        .catch(err => done(err))
+
+      })
+    })
+
+  })
