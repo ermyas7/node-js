a01fe907c4eac9f681c680e9b4bcfe44252daaad
ermyas7 <ermyas.fekadu@gmail.com>
2019-02-23 21:11:04 +0530

    Add mustache.js for message template

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -5,10 +5,14 @@ socket.on('connect', function(message) {
 })
 
 socket.on('newMessage', function(message) {
+  var template = jQuery('#message-template').html();
   var formatedDate = moment(message.createdAt).format('h:mm a');
-  var li = jQuery(`<li></li>`);
-  li.text(`${message.from} ${formatedDate} : ${message.text}`);
-  jQuery('#messages').append(li);
+  var html  = Mustache.render(template, {
+    from: message.from,
+    createdAt: formatedDate,
+    text: message.text
+  });
+  jQuery('#messages').append(html);
 })
 
 socket.on('disconnect', function() {
@@ -49,12 +53,12 @@ console.log(position)
 })
 
 socket.on('newLocationMessage', function(message){
+  var template = jQuery('#location-message-template').html();
   var formatedDate = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
-  li.text(`${message.from} ${formatedDate}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
-
+  var html  = Mustache.render(template, {
+    from: message.from,
+    createdAt: formatedDate,
+    url: message.url
+  });
+  jQuery('#messages').append(html);
 })
